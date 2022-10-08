f12ce9c376f38795e2bf458fec7e77bdda24b4f5
lilou09 <loverdo123@gmail.com>
2022-10-08 22:40:54 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,25 @@
+const markAsUnread = document.getElementById("markunread");
+const unread = document.querySelectorAll(".unread");
+const countH = document.querySelector(".number");
+
+function removeDots(elem) {
+  const dot = elem.querySelector("* > .red_dot");
+  dot.remove();
+}
+
+markAsUnread.addEventListener("click", () => {
+  countH.textContent = 0;
+  unread.forEach((elem) => {
+    elem.classList.remove("unread");
+  });
+});
+unread.forEach((elem) => {
+  if (countH.value !== 0) {
+    console.log(countH.value);
+    elem.addEventListener("click", () => {
+      elem.classList.remove("unread");
+      removeDots(elem);
+      countH.textContent--;
+    });
+  }
+});
